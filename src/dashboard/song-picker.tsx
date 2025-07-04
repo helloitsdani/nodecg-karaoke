64905c04a37d3b2703b33974b30cde647640b798
dani quinn sexton <dani@glitter.tech>
2025-07-05 00:41:35 +0100

    feat: Initial version of panel and overlay

---
+++ src/dashboard/song-picker.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const rootEl = document.getElementById("root");
+const root = ReactDOM.createRoot(rootEl!);
+
+root.render(
+  <React.StrictMode>
+    <div>
+      <h1>put a nice song on</h1>
+    </div>
+  </React.StrictMode>
+)
---
+++ src/graphics/karaoke-machine.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const rootEl = document.getElementById("root");
+const root = ReactDOM.createRoot(rootEl!);
+
+root.render(
+  <React.StrictMode>
+    <div>
+      <h1>🎶</h1>
+    </div>
+  </React.StrictMode>
+)
